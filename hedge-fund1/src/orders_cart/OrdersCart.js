120cc28839dad3bfd32290e41e0bb83a25b44b7e
mykolayes <32031506+mykolayes@users.noreply.github.com>
2019-05-04 14:01:16 +0300

    Fixes.

--- hedge-fund1/src/orders_cart/OrdersCart.js
+++ hedge-fund1/src/orders_cart/OrdersCart.js
@@ -18,10 +18,9 @@ class OrdersCart extends Component {
                 (Algorobot should be placed somewhere here)
 
                 <div className="codegena_iframe">
-                    <iframe src="http://mses.kpi.ua/kursovi2/zinchenko.pdf" height="400" width="600"></iframe>
+                    <iframe src="http://mses.kpi.ua/kursovi2/zinchenko.pdf" height="800" width="800"></iframe>
                 </div>
-                {/*<style>.codegena_iframe{position:relative;padding-bottom:56.25%;height:0;overflow: hidden;max-width:100%;}.codegena_iframe*/}
-                    {/*iframe{position:absolute;top:0;left:0;width:100%;height:100%;}</style>*/}
+
             </div>
         );
     }
